42bd682b41e27d134fe7a2c33386d452292b734d
5bit <cuinmanari@gmail.com>
2016-03-02 14:00:29 -0600

    Setting up git
    Setting up git in this initial push.
    Also created a main class.

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Main {
+	// Version number, in case I update this more.
+	static String versionNumber = "V. 1.0";
+	
+	/**
+	 * Main function - where all good things start
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Starting Name Generator");
+		System.out.println(versionNumber);
+		System.out.println("Link the .txt file containing names.");
+		
+		
+	}
+	
+	
+	
+	
+	
+}
